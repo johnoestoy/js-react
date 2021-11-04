a9413422308e5ddb47bf7be39833ca4e2f32ac24
John <johnoestoy@gmail.com>
2021-11-04 09:48:29 +0000

    :construction: Create BoardGame Objectt without css

---
+++ src/objects/BoardGame/index.js
@@ -0,0 +1,8 @@
+import CardGame from "../../components/CardGame"
+
+function BoardGame(amountCard){
+  const $htmlCardGame = CardGame();
+  const $htmlBoardGame = $htmlCardGame.repeat(amountCard);
+  return $htmlBoardGame
+};
+export default BoardGame;
