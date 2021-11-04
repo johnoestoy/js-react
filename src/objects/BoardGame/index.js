c984a47baa99714bb2c3fe8cb29c21dbac31b5c1
John <johnoestoy@gmail.com>
2021-11-04 10:20:04 +0000

    :sparkles: Finish the BoardGame object

--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -1,8 +1,13 @@
+import "./style.css"
 import CardGame from "../../components/CardGame"
 
 function BoardGame(amountCard){
   const $htmlCardGame = CardGame();
-  const $htmlBoardGame = $htmlCardGame.repeat(amountCard);
-  return $htmlBoardGame
+  const $htmlContent = $htmlCardGame.repeat(amountCard);
+  return `
+  <section class="board-game">
+    ${$htmlContent}
+  </section>
+  `
 };
 export default BoardGame;
