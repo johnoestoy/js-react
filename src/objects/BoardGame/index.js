e9a3ea631fe0758233bc70f533dabd0f5c1dcc9d
John <johnoestoy@gmail.com>
2021-11-05 10:12:59 +0000

    :lipstick: Create ScoreBoard object

--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -4,7 +4,7 @@ import CardGame from "../../components/CardGame"
 function BoardGame(amountCard){
   const $htmlCardGame = CardGame();
   const $htmlContent = $htmlCardGame.repeat(amountCard);
-  return `
+  return /*html*/`
   <section class="board-game">
     ${$htmlContent}
   </section>
